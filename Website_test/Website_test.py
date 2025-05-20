e14eade9650bb9351fa96226118a8b9842ade15b
Racoon <Superdobriyden@yandex.ru>
2025-05-20 15:05:23 +0300

    text_correct

--- Website_test/Website_test.py
+++ Website_test/Website_test.py
@@ -8,13 +8,10 @@ from webdriver_manager.firefox import GeckoDriverManager
 driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
 driver.get("https://example.com")
-# driver.find_element(By.LINK_TEXT, "More information...").click()
 title = driver.title
 sub = "Example"
 orig_site = "https://www.iana.org/help/example-domains"
-sss = "body > div:nth-child(1) > p:nth-child(3) > a:nth-child(1)"
 
-#Проверка заголовка на наличие "Example"
 if sub in title:
     driver.find_elements(By.CSS_SELECTOR, "#body > div:nth-child(1) > p:nth-child(3) > a:nth-child(1)")
     driver.find_element(By.LINK_TEXT, "More information...").click()
@@ -23,7 +20,7 @@ if sub in title:
     else:
         print("Кажется, сайт обновили, это не он.")
 else:
-    print("Неверный сайт!")
+    print("Неверный сайт, заголовок не совпал!")
 
 time.sleep(5)
 driver.quit()
