8252f455ed94ced7006f5b8fa9d5b05cbe8c65b0
Racoon <Superdobriyden@yandex.ru>
2025-05-20 20:32:50 +0300

    All

--- Website_test/Website_test.py
+++ Website_test/Website_test.py
@@ -1,6 +1,6 @@
-from selenium import webdriver
 import time
 
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
