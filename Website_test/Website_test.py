5aa3555c7293573fc018a5f7e5be5335c3a787aa
Racoon <Superdobriyden@yandex.ru>
2025-05-20 11:53:33 +0300

    Проверка сайта

--- Website_test/Website_test.py
+++ Website_test/Website_test.py
@@ -8,22 +8,23 @@ from webdriver_manager.firefox import GeckoDriverManager
 driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
 driver.get("https://example.com")
-
+# driver.find_element(By.LINK_TEXT, "More information...").click()
 title = driver.title
 sub = "Example"
 orig_site = "https://www.iana.org/help/example-domains"
-
+sss = "body > div:nth-child(1) > p:nth-child(3) > a:nth-child(1)"
 
 #Проверка заголовка на наличие "Example"
 if sub in title:
-    driver.find_element(By.LINK_TEXT, "More information...").click() #ПРОВЕРЯЕТ ЭЛЕМЕНТ И КЛИКАЕТ ПО НЕМУ
-    if driver.current_url == orig_site:                              #ПРОВЕРЯЕТ НА КАКОЙ САЙТ МЫ ПЕРЕШЛИ
+    driver.find_elements(By.CSS_SELECTOR, "#body > div:nth-child(1) > p:nth-child(3) > a:nth-child(1)")
+    driver.find_element(By.LINK_TEXT, "More information...").click()
+    if driver.current_url == orig_site:
         print("Вы перешли на правильный сайт!")
     else:
-        print("Кажется, сайт обновили, это не он!")
+        print("Кажется, сайт обновили, это не он.")
 else:
     print("Неверный сайт!")
 
 time.sleep(5)
 driver.quit()
-print("Готово!")
+print("Программа завершена!")
