685659c01b1abc5ddc97521e485703b4137c9736
Racoon <Superdobriyden@yandex.ru>
2025-05-20 20:38:53 +0300

    All.

--- Website_test/Website_test.py
+++ Website_test/Website_test.py
@@ -1,9 +1,9 @@
-import time
-
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.firefox.service import Service
 from webdriver_manager.firefox import GeckoDriverManager
+
+import time
 
 driver = webdriver.Firefox(service=Service(GeckoDriverManager().install()))
 
